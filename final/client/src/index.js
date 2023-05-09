c5f99a8dc7d9049417fe3a74b066c4273acfc183
Wei Zhang <missweizhang@gmail.com>
2023-05-09 08:45:58 -0700

    update server uri

--- final/client/src/index.js
+++ final/client/src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://apollo-odyssey-production.up.railway.app/', // 'http://localhost:4000',
   cache: new InMemoryCache(),
 });
 
