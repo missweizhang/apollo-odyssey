7c4808049cfcf87651b8c2f6303bfc758364aea3
Wei Zhang <missweizhang@gmail.com>
2023-05-08 20:27:22 -0700

    turn on introspection

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,7 @@ const resolvers = require("./resolvers");
 const TrackAPI = require("../datasources/track-api");
 
 async function startApolloServer() {
-  const server = new ApolloServer({ typeDefs, resolvers });
+  const server = new ApolloServer({ typeDefs, resolvers, introspection: true });
   const { url } = await startStandaloneServer(server, {
     context: async () => {
       const { cache } = server;
